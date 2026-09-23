fd5182745eeeb49af18ca33dd5ff061ca5fd304a
leezoonza <leezoonza@gmail.com>
2026-09-23 23:31:39 +0900

    test(modulith): 아키텍처 테스트 표시명 제거
    - 모듈 경계 검증 테스트의 표시명 어노테이션을 제거
    - 아키텍처 검증 동작은 그대로 유지

--- src/test/java/com/zoonza/pay/ModulithArchitectureTests.java
+++ src/test/java/com/zoonza/pay/ModulithArchitectureTests.java
@@ -1,6 +1,5 @@
 package com.zoonza.pay;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 
@@ -8,7 +7,6 @@ class ModulithArchitectureTests {
     private final ApplicationModules modules = ApplicationModules.of(PayApplication.class);
 
     @Test
-    @DisplayName("애플리케이션 모듈 경계와 의존성 규칙을 준수한다")
     void verifiesModuleStructure() {
         modules.verify();
     }
